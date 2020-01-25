dc9d04bb067d06815f5d1e92c137b19db9f8baf3
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2020-01-25 12:23:36 -0800

    Delete environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,7 @@ const axios = require('axios').default;
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:4000',
+  baseURL: 'http://140.161.84.36:4000',
   timeout: 3000,
   validateStatus: function(status) {
     if(status >= 200 && status < 400) {
