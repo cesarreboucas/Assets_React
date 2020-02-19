f6b5a5100fc9b7aed0e45a4636c49eda7a606c98
cesarreboucas <cesar.reboucas@gmail.com>
2020-02-18 19:33:02 -0800

    Goals details showing correctly

--- src/components/goals/goalItemForm.js
+++ src/components/goals/goalItemForm.js
@@ -4,23 +4,23 @@ import DatePicker from 'react-datepicker';
 class GoalItemForm extends React.Component {
   constructor(props) {
     super(props);
-
-    if(this.props.data===undefined) {
+    if (this.props.data === undefined) {
       this.state = {
-        description:'',
-        periodicity:"once",
-        value:0,
+        description: '',
+        periodicity: "once",
+        value: 0,
         dateStart: new Date(),
         dateEnd: new Date(),
-        interestRate:0,
-        useIRR:false
+        interestRate: 0,
+        useIRR: false
       }
     } else {
-      this.state = this.props.data
+       this.state = this.props.data;
     }
     
+
   }
-  
+
   handleInputChange = evt => {
     const target = evt.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
@@ -30,8 +30,20 @@ class GoalItemForm extends React.Component {
     });
   }
 
+  handleFormNumber = (field,value) => {
+    this.setState({[field]: value});
+  }
+
+  handleSelectPeriodicity = (e) => {
+    if(e.target.value==="once") {
+      this.setState({dateEnd: this.state.dateStart, periodicity:e.target.value});
+    } else {
+      this.setState({periodicity:e.target.value});
+    }
+  }
+
   updateDates = (box, date) => {
-    if(box===0) { //Start
+    if (box === 0) { //Start
       if(date > this.state.dateEnd) { //It cant start after end
         this.setState({dateStart: date, dateEnd:date});
       } else {
@@ -51,24 +63,23 @@ class GoalItemForm extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     return (
       <div style={{ backgroundColor: "white", padding: "20px 50px" }}>
         <div className="form-row" >
-        <h3>{this.state.description===''?'Collumn '+(this.props.n+1):this.state.description}</h3>
+          <h3>{this.state.description === '' ? 'Collumn ' + (this.props.n + 1) : this.state.description}</h3>
         </div>
         <div className="form-row" >
           <div className="form-group col-md-4">
             <label>Name</label>
-            <input type="text" className="form-control" name="description" 
-              defaultValue={this.state.description} 
+            <input type="text" className="form-control" name="description"
+              defaultValue={this.state.description}
               onBlur={this.handleInputChange} />
           </div>
           <div className="form-group col-md-4">
             <label>Periodicity</label>
             <select className="form-control" name="periodicity"
-              defaultValue={this.state.periodicity} 
-              onChange={this.handleInputChange}>
+              defaultValue={this.state.periodicity}
+              onChange={this.handleSelectPeriodicity}>
               <option value="once">Once</option>
               <option value="monthly">Monthly</option>
               <option value="bimonthly">Every Two Months</option>
@@ -79,19 +90,19 @@ class GoalItemForm extends React.Component {
             </select>
           </div>
           <div className="form-group col-md-4">
-          <label>Value</label>
-            <input type="number" style={{ textAlign: 'right' }} className="form-control" name="value" 
-              defaultValue={this.state.value} 
-              onBlur={this.handleInputChange} />
+            <label>Value</label>
+            <input type="number" style={{ textAlign: 'right' }} className="form-control" name="value"
+              defaultValue={this.state.value}
+              onBlur={(e) => {this.handleFormNumber("value", parseFloat(e.target.value))}} />
           </div>
         </div>
         <div className="form-row">
           <div className="form-group col-md-4">
             <label>Yearly Interest Rate (%)</label>
-            <input type="number" step="0.01" style={{ textAlign: 'right' }} className="form-control" name="interestRate" 
+            <input type="number" step="0.01" style={{ textAlign: 'right' }} className="form-control" name="interestRate"
               disabled={this.state.useIRR}
-              defaultValue={this.state.interestRate} 
-              onBlur={this.handleInputChange} />
+              defaultValue={this.state.interestRate}
+              onBlur={(e) => {this.handleFormNumber("interestRate", parseFloat(e.target.value))}} />
           </div>
           <div className="form-group col-md-4">
             <label>Start Date</label>&nbsp;
@@ -106,7 +117,7 @@ class GoalItemForm extends React.Component {
 						<DatePicker className="form-control"
               selected={this.state.dateEnd}
               showMonthYearPicker
-              disabled = {this.state.periodicity==="once"? true:false}
+              disabled={this.state.periodicity === "once" ? true : false}
               minDate={this.state.dateStart}
               onChange={(date) => { this.updateDates(1, date) }}
               dateFormat="MMMM yyyy" />
@@ -114,11 +125,11 @@ class GoalItemForm extends React.Component {
         </div>
         <div className="form-group">
           <div className="form-check">
-            <input className="form-check-input" type="checkbox" name="useIRR" 
+            <input className="form-check-input" type="checkbox" name="useIRR"
               onChange={this.handleInputChange} defaultChecked={this.state.useIRR} />
             <label className="form-check-label">
               Use my computed return <small>(the Yearly return from Assets)</small>
-						</label>
+            </label>
           </div>
         </div>
       </div>
--- src/components/goals/goalsDetail.js
+++ src/components/goals/goalsDetail.js
@@ -1,64 +1,72 @@
 import React from 'react';
 import GoalItemForm from './goalItemForm';
 
-class GoalsDetail extends React.Component {
+export default class GoalsDetail extends React.Component {
 
   state = {
     nOfBoxes: 1,
-    showTable:false,
-    dataLoaded:"0x"
+    showTable: false,
+    dataLoaded: 0
   }
 
-  data = [];
+  data = {
+    options: {},
+    boxes: []
+  };
 
   renderBoxes = () => {
     let boxes = [];
     for (let i = 0; i < this.state.nOfBoxes; ++i) {
-      boxes.push(<GoalItemForm n={i} key={this.state.dataLoaded+"_"+i} saveData={this.saveData} data={this.data[i]} />)
+      boxes.push(<GoalItemForm n={i} key={this.state.dataLoaded + "_" + i} saveData={this.saveDataFromBoxes} data={this.data.boxes[i]} />)
     }
     return boxes;
   }
 
-  saveData = (data, i) => {
-    this.data[i] = data;
-  }
-
-  renderTable = () => {
-    console.log(this.data);
-    let minDate = new Date();
-    let maxDate = new Date();
-
-    for(let i=0; i < this.data.length; ++i) {
-      if(minDate > this.data[i].dateStart) { minDate = this.data[i].dateStart}
-      if(maxDate < this.data[i].dateEnd) { minDate = this.data[i].dateEnd}
-    }
-    let currentMonth = minDate;
-    currentMonth.setDate(10);
-    console.log(currentMonth);
+  saveDataFromBoxes = (data, i) => {
+    this.data.boxes[i] = data;
   }
 
   componentDidMount() {
-    this.data = [
-      {
-        description:'Salary',
-        periodicity:"monthly",
-        value:950,
-        dateStart: new Date('2018-12-01'),
-        dateEnd: new Date('2024-12-30'),
-        interestRate:2,
-        useIRR:false
-      },
-      {
-        description:'Rent',
-        periodicity:"monthly",
-        value:-1500,
-        dateStart: new Date('2018-09-01'),
-        dateEnd: new Date('2025-12-30'),
-        interestRate:1,
-        useIRR:false
-      }
-    ]
-    this.setState({dataLoaded:"1x", nOfBoxes:this.data.length});
+    this.data = {
+      options: {},
+      boxes: [
+        {
+          description: 'Salary',
+          periodicity: "monthly",
+          value: 950,
+          dateStart: "2017-12-10T00:22:15.357Z",
+          dateEnd: "2024-12-10T00:22:16.357Z",
+          interestRate: 2,
+          useIRR: false
+        },
+        {
+          description: 'Rent',
+          periodicity: "monthly",
+          value: -1500,
+          dateStart: "2018-09-10T00:22:17.357Z",
+          dateEnd: "2025-12-10T00:22:18.357Z",
+          interestRate: 1,
+          useIRR: false
+        },
+        {
+          description: 'Gov Help',
+          periodicity: "quaterly",
+          value: 200,
+          dateStart: "2020-09-10T00:22:00.357Z",
+          dateEnd: "2025-12-10T00:22:00.357Z",
+          interestRate: 1,
+          useIRR: false
+        }
+      ]
+    };
+    //Converting toIsoStrings to Date Obj
+    for(let i=0; i < this.data.boxes.length; ++i) {
+      const box = this.data.boxes[i];
+      box.dateStart = new Date(this.data.boxes[i].dateStart);
+      box.dateEnd = new Date(this.data.boxes[i].dateEnd);
+    }
+    //CHANGE SHOWTABLE
+    this.setState({ dataLoaded: 1, nOfBoxes: this.data.boxes.length, showTable: true });
   }
 
   render() {
@@ -68,27 +76,167 @@ class GoalsDetail extends React.Component {
         {
           this.renderBoxes()
         }
-        <div className="form-group text-right" style={{padding:"15px", backgroundColor:"#ffffff", margin:"0px"}}>
-          <button type="button" className="btn btn-primary" 
-            onClick={() => {this.setState({nOfBoxes: (this.state.nOfBoxes+1)})}} 
-            >Add Collumn</button>
-        </div>
-        <div className="form-group text-right" style={{padding:"15px", backgroundColor:"#ffffff"}}>
+        <div className="form-group text-right" style={{ padding: "15px", backgroundColor: "#ffffff", margin: "0px" }}>
           <button type="button" className="btn btn-primary"
-            onClick={()=> this.setState({showTable: !this.state.showTable})}>{this.state.showTable?'Hide':'Show'}</button>&nbsp;
-          <button type="button" className="btn btn-primary">Save</button>
+            onClick={() => { this.setState({ nOfBoxes: (this.state.nOfBoxes + 1) }) }}
+          >Add Collumn</button>
         </div>
-        { this.state.showTable && 
+        <div className="form-group text-right" style={{ padding: "15px", backgroundColor: "#ffffff" }}>
+          <button type="button" className="btn btn-primary" style={{ margin: "4px"}}
+            onClick={() => this.setState({ showTable: !this.state.showTable })}>{this.state.showTable ? 'Hide Table' : 'Show Table'}</button>
+          {
+            (this.state.showTable?
+              <button type="button" className="btn btn-primary" style={{ margin: "4px"}}
+                onClick={() => this.setState({ dataLoaded: this.state.dataLoaded+1 })}>Update</button>:'')
+          }            
+          <button type="button" className="btn btn-primary" style={{ margin: "4px"}}>Save</button>
+        </div>
+        {this.state.showTable &&
           <div className="form-row">
             {
-              this.renderTable()
+              <GoalsTable data={this.data} />
             }
           </div>
         }
       </div>
-      
+
     );
   }
 }
 
-export default GoalsDetail;
+class GoalsTable extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.boxes = this.props.data.boxes;
+    this.options = this.props.data.options;
+    this.result = 0;
+  }
+
+  renderTable = () => {
+    this.result = 0;
+    let rows = []
+    let minDate = new Date();
+    let maxDate = new Date();
+
+    let headingTds = [<th key="thMonth">Month</th>];
+    for (let i = 0; i < this.boxes.length; ++i) {
+      headingTds.push(<th style={{textAlign:"right"}} key={"th"+i}>{this.boxes[i].description}</th>);
+      if (minDate > this.boxes[i].dateStart) { minDate = this.boxes[i].dateStart }
+      if (maxDate < this.boxes[i].dateEnd) { maxDate = this.boxes[i].dateEnd }
+    }
+    headingTds.push(<th style={{textAlign:"right"}} key={"thResult"}>Result</th>);
+
+    //Cloning the date
+    let currentMonth = new Date(minDate.getTime());
+    // Picking any day since the month is most important
+    currentMonth.setDate(10);
+    maxDate.setDate(10);
+    rows.push(<tr key="trHeading">{headingTds}</tr>)
+
+    while (this.evaluateDate(maxDate, currentMonth) >= 0) {
+      rows.push(this.renderTr(currentMonth));
+      //Updating current month
+      if (currentMonth.getMonth() === 11) {
+        currentMonth.setMonth(0);
+        currentMonth.setFullYear(currentMonth.getFullYear() + 1);
+      } else {
+        currentMonth.setMonth(currentMonth.getMonth() + 1);
+      }
+    }
+    return rows;
+  }
+
+  renderTr = (month) => {
+    const MonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const trHeader = MonthNames[month.getMonth()] + " " + month.getFullYear();
+    const tds = [<th key={trHeader}>{trHeader}</th>];
+
+    this.boxes.forEach((box, i) => {
+      let value = box.value;
+      //Current after Start and before end
+      if (this.evaluateDate(month, box.dateStart)>=0 && this.evaluateDate(month, box.dateEnd)<=0) {
+        let periodicity;
+        switch(box.periodicity) {
+          case "monthly":
+            periodicity = 1;
+            break;
+          case "bimonthly":
+            periodicity = 2;
+            break;
+          case "quaterly":
+            periodicity = 3;
+            break;
+          case "fourmonth":
+            periodicity = 4;
+            break;
+          case "biyearly":
+            periodicity = 6;
+            break;
+          case "yearly":
+            periodicity = 12;
+            break;
+          default:
+            periodicity = 1;
+            break;
+        }
+        if (box.dateStart.getMonth() % periodicity === month.getMonth() % periodicity) {
+          value = value * Math.pow(1 + (box.interestRate / 100), this.getExponential(box.dateStart, month));
+          this.result += value;
+          tds.push(<td key={trHeader + i} style={{textAlign:"right"}}>
+              {value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+            </td>);
+        } else { // No value for this current month
+          tds.push(<td key={trHeader + i}></td>); 
+        }
+      } else { //out of interval
+        tds.push(<td key={trHeader + i}></td>); 
+      }
+    });
+
+    tds.push(<td key={trHeader + "result"} style={{textAlign:"right"}}>
+      {this.result.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</td>);
+    return (
+      <tr key={month.getFullYear() + "-" + month.getMonth()}>
+        {tds}
+      </tr>
+    );
+  }
+
+  getExponential = (dateA, dateB) => {
+    let e = dateB.getFullYear() - dateA.getFullYear();
+    if(dateB.getMonth() < dateA.getMonth()) { // Not a complete year
+      --e;
+    }
+    return(e);
+  }
+
+  /**
+   *  1 A Bigger, 
+   *  0 Equal, 
+   * -1 B Bigger, 
+   */
+  evaluateDate = (dateA, dateB) => {
+    if (dateA.getFullYear() > dateB.getFullYear()) { //Year A greater
+      return 1;
+    } else if (dateA.getFullYear() < dateB.getFullYear()) { //Year B greater
+      return -1;
+    } else if (dateA.getMonth() > dateB.getMonth()) { //Same Year, Month A greater
+      return 1;
+    } else if(dateA.getMonth() < dateB.getMonth()) { //Same Year, Month B greater
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  render() {
+    return (
+      <table className="table table-dark" style={{width:"90%",margin:"auto"}}>
+        <tbody>
+          {this.renderTable()}
+        </tbody>
+      </table>
+    );
+  }
+}
